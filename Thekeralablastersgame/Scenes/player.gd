ef6a8e290f4c893340ec447f19b2a08d609da97c
KeralaBlasters <isatho25@kingshigh.school.nz>
2024-07-23 10:00:53 +1200

    Worked on enemy and player
    Fixed the player's movement. I also started making the enemy

--- Thekeralablastersgame/Scenes/player.gd
+++ Thekeralablastersgame/Scenes/player.gd
@@ -11,16 +11,16 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("left","right","up","down").normalized()
+	var direction: = Input.get_vector("left","right","up","down").normalized()
 	if direction:
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+	
 	look_at(get_global_mouse_position())
 	var move_input = Input.get_axis("down", "up")
 	var rotation_direction = Input.get_axis("left", "right")
 	velocity = transform.x * move_input * SPEED
-	rotation += rotation_direction * ROTATION_SPEED * delta
 	
 
 	move_and_slide()
