62aa5fdda579f7cf58daddcdd129f9717c6cb40d
KeralaBlasters <isatho25@kingshigh.school.nz>
2024-08-21 13:27:25 +1200

    Worked on the enemy
    I started coding the enemy AI. I made it so that the enemy will start chasing the player if it goes in the enemy's view.

--- Thekeralablastersgame/Scenes/enemy.gd
+++ Thekeralablastersgame/Scenes/enemy.gd
@@ -1,2 +1,22 @@
 extends CharacterBody2D
 
+@export var enemy_speed = 50
+var attacking = false
+var player = null
+
+func _physics_process(delta):
+	if attacking:
+		position += (player.position - position)/enemy_speed
+		
+
+
+
+func _on_detect_player_area_body_entered(body):
+	player = body
+	attacking = true
+
+
+
+func _on_detect_player_area_body_exited(body):
+	player = null
+	attacking = false
