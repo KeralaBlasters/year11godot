279d5a1da83d4f7603d8b4998afedf2368ed67f9
KeralaBlasters <isatho25@kingshigh.school.nz>
2024-08-27 10:13:02 +1200

    Worked on the title screen and the enemy
    I started making the title screen and worked on making the enemy attack the player.

--- Thekeralablastersgame/Scenes/enemy.gd
+++ Thekeralablastersgame/Scenes/enemy.gd
@@ -4,16 +4,44 @@ extends CharacterBody2D
 var attacking = false
 var player = null
 
+@onready var animation_player = $AnimationPlayer
+
+@export var player_look_at: NodePath
+
+@export var Enemy_bullet = PackedScene
+
+enum state {IDLE, ATTACK}
+var enemy_anim_state = state.IDLE
+
 func _physics_process(delta):
 	if attacking:
 		position += (player.position - position)/enemy_speed
-		
+		enemy_anim_state = state.ATTACK
+	else:
+		enemy_anim_state = state.IDLE
+	
+	#if _on_detect_player_area_body_entered(player):
+		#var c = Enemy_bullet.instantiate()
+		#owner.add_child(c)
+		#c.transform = $EnemyMuzzle.global_transform
+	
+	if player:
+		look_at(player.global_position)
+	
+	update_enemy_animation()
 
-
+func update_enemy_animation():
+	match enemy_anim_state:
+		state.IDLE:
+			$AnimationPlayer.play("idle")
+		state.ATTACK:
+			$AnimationPlayer.play("attack")
+			
 
 func _on_detect_player_area_body_entered(body):
 	player = body
 	attacking = true
+	
 
 
 
