b063e0b4de604ab07982ae2096c45588418be3f1
KeralaBlasters <isatho25@kingshigh.school.nz>
2024-09-04 11:00:49 +1200

    Worked on levelling up the player
    I added some code required for levelling up the player. I also started adding a notice to level up.

--- Thekeralablastersgame/Singletons/bullet_stats.gd
+++ Thekeralablastersgame/Singletons/bullet_stats.gd
@@ -1,4 +1,23 @@
 extends Node
 
-
+var bullet_level = 1
 var bullet_damage = 5
+var bullet_speed = 500
+var bullet_projectiles = 1
+var bullet_knockback = 10
+
+func level_up_knife():
+	bullet_level += 1
+	match bullet_level:
+		1:
+			pass
+		2:
+			bullet_damage = 10
+			bullet_speed = 550
+			bullet_projectiles = 2
+			bullet_knockback = 15
+		3:
+			bullet_damage = 20
+			bullet_speed = 600
+			bullet_projectiles = 3
+			bullet_knockback = 20
--- Thekeralablastersgame/Singletons/player_stats.gd
+++ Thekeralablastersgame/Singletons/player_stats.gd
@@ -2,3 +2,19 @@ extends Node
 
 var player_health = 100
 var player_max_health = 100
+var player_level = 1
+var player_experience = 0
+var next_level = 15
+
+signal level_up
+
+func level_up_player():
+	player_level += 1
+	next_level = next_level + 10
+	player_experience = 0
+	emit_signal("level_up")
+
+func add_experience(val):
+	player_experience += val
+	if player_experience >= next_level:
+		level_up_player()
