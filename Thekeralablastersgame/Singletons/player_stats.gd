1c6e3e90638a7db1d62db28bc6da94f9e849b4a3
KeralaBlasters <isatho25@kingshigh.school.nz>
2024-09-16 13:28:23 +1200

    I worked on the player and the UI
    I worked on making the player take damage and I worked on adding a timer to the screen for having high scores.

--- Thekeralablastersgame/Singletons/player_stats.gd
+++ Thekeralablastersgame/Singletons/player_stats.gd
@@ -7,6 +7,8 @@ var player_experience = 0
 var next_level = 15
 
 signal level_up
+signal add_xp
+signal take_damage
 
 func level_up_player():
 	player_level += 1
@@ -16,5 +18,11 @@ func level_up_player():
 
 func add_experience(val):
 	player_experience += val
+	emit_signal("add_xp")
 	if player_experience >= next_level:
 		level_up_player()
+
+
+func damage_player(amount):
+	player_health -= amount
+	emit_signal("take_damage")
