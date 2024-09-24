f41d5ab28de8ecf2dd3b70f8bfc38f0343387282
KeralaBlasters <isatho25@kingshigh.school.nz>
2024-09-24 16:08:24 +1200

    I worked on level 2.
    I worked on the tilemap for level 2.

--- Thekeralablastersgame/Scenes/Levels/level_1.gd
+++ Thekeralablastersgame/Scenes/Levels/level_1.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var audio_player = $AudioStreamPlayer2D
 
+const level_2 = "res://Scenes/Levels/level_2.tscn"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -14,7 +16,3 @@ func _process(delta: float) -> void:
 
 func _enter_tree():
 	MusicPlayer.playcalmer()
-
-
-#func _on_go_to_level_2_body_entered(Player):
-#		get_tree().change_scene_to_file("res://Scenes/Levels/Level2.tscn")
