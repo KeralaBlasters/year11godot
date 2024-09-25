ee3763ae5ee416d061e41f6c3f4e639179e2b830
KeralaBlasters <isatho25@kingshigh.school.nz>
2024-09-25 15:22:12 +1200

    I almost completed my project.
    I made it so that you can go to the next level when you finish a level.

--- Thekeralablastersgame/Scenes/Levels/level_1.gd
+++ Thekeralablastersgame/Scenes/Levels/level_1.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @onready var audio_player = $AudioStreamPlayer2D
 
-const level_2 = "res://Scenes/Levels/level_2.tscn"
+const level_2 = "res://Scenes/Levels/Level2.tscn"
+const level_3 = "res://Scenes/Levels/Level3.tscn"
+const you_win = "res://Scenes/Levels/you_win.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,3 +18,18 @@ func _process(delta: float) -> void:
 
 func _enter_tree():
 	MusicPlayer.playcalmer()
+	
+
+func _on_go_to_level_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file(level_2)
+		
+
+func _on_go_to_level_3_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file(level_3)
+		
+
+func _on_go_to_win_screen_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file(you_win)
